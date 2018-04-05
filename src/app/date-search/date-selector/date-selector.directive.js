157a070a64a42496c4c31302a443fd429c36817f
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2018-04-05 00:16:10 -0600

    dynamically generated select dropdowns

--- src/app/date-search/date-selector/date-selector.directive.js
+++ src/app/date-search/date-selector/date-selector.directive.js
@@ -6,13 +6,52 @@ function dateSelectorDirective() {
     scope: {},
     template,
     controller: DateSelectorController,
-    controllerAs: 'vm',
+    controllerAs: 'ds',
   };
 }
 
 // DateSelectorController.$inject = ['marvelService'];
 
-function DateSelectorController() {
+class DateSelectorController {
+  constructor() {
+    const today = new Date();
+    this.currentMonth = today.getMonth();
+    this.currentYear = today.getYear();
+    this.currentDate = today.getDate();
+    this.months = [
+      { MM: '01', name: 'January' },
+      { MM: '02', name: 'February' },
+      { MM: '03', name: 'March' },
+      { MM: '04', name: 'April' },
+      { MM: '05', name: 'May' },
+      { MM: '06', name: 'June' },
+      { MM: '07', name: 'July' },
+      { MM: '08', name: 'August' },
+      { MM: '09', name: 'September' },
+      { MM: '10', name: 'October' },
+      { MM: '11', name: 'November' },
+      { MM: '12', name: 'December' },
+    ];
+    this.selectedMonth = this.months[this.currentMonth];
+    this.days = this.getDaysInMonth(this.selectedMonth, this.currentYear);
+    this.selectedDay = this.currentDate;
+  }
+
+  setDaysByMonth(month) {
+    this.days = this.getDaysInMonth(month, this.currentYear);
+  }
+
+  getDaysInMonth(monthObj, year) {
+    const month = parseInt(monthObj.MM, 10) - 1;
+    const date = new Date(year, month, 1);
+    const days = [];
+
+    while (date.getMonth() === month) {
+      days.push(new Date(date).getDate());
+      date.setDate(date.getDate() + 1);
+    }
+    return days;
+  }
 }
 
 export default dateSelectorDirective;
