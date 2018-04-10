40a1cc53164cd4f07c29aac892c37284a1c904dc
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2018-04-10 17:16:19 -0600

    data flows to comic preview directive

---
+++ src/app/date-search/results/comic-preview.directive.js
@@ -0,0 +1,23 @@
+import template from './comic-preview.html';
+
+function comicPreviewDirective() {
+  return {
+    restrict: 'E',
+    scope: {
+      issue: '='
+    },
+    template,
+    controller: ComicPreviewController,
+    controllerAs: 'cp',
+  };
+}
+
+class ComicPreviewController {
+  constructor($scope) {
+    'ngInject';
+
+    this.issue = $scope.issue;
+  }
+}
+
+export default comicPreviewDirective;
